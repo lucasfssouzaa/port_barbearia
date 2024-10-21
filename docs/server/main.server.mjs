b512de6684099dd6e9564fdb23aec52cec9a4730
lucasfssouzaa <lucasfsouza43@gmail.com>
2024-10-21 13:44:58 -0300

    ajuste na responsividade

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-I6Q6J2UC.mjs";import"./chunk-4SCZQ5OO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-SP2HLA45.mjs";import"./chunk-4SCZQ5OO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
